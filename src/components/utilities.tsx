bb8eec49c1ea21e2627161408f044285c4f360d7
nyinyi979 <nyinyi095062687@gmail.com>
2023-11-30 20:51:49 +0630

    utilities

--- src/components/utilities.tsx
+++ src/components/utilities.tsx
@@ -128,6 +128,6 @@ export const bg1Class = `[background-image:url('/bg1.jpg')] bg-fixed md:bg-cente
 export const bg3Class = `[background-image:url('/bg3.jpg')] bg-scroll bg-center bg-repeat bg-cover`
 export const bg2Class = `[background-image:url('/bg2.jpg')] bg-center bg-cover bg-no-repeat`
 export const bg4Class = `[background-image:url('/bg4.jpg')] md:bg-center lg:[background-size: 95%] sm:bg-center xs:bg-[center_left_-250px] bg-[center_left_-300px] bg-repeat bg-cover `
-export const bg5Class = `[background-image:url('/img5.jpg')] bg-fixed md:[background-position:center top] bg-[center_left_-400px] bg-repeat bg-cover`
+export const bg5Class = `[background-image:url('/img5.jpg')] bg-fixed ssm:[background-position:center top] bg-[center_left_-750px] bg-repeat bg-cover`
 export const bg6Class = `[background-image:url('/bg6.png')] bg-center bg-repeat bg-cover`
 export const bg7Class = `[background-image:url('/bg7.png')] bg-center bg-cover bg-scroll`
