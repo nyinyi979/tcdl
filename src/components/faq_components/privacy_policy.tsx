531e1d321119d00c92d415b02f5dcca8e0ef6045
nyinyi979 <nyinyi095062687@gmail.com>
2023-12-06 12:27:48 +0630

    privacy_policy

--- src/components/faq_components/privacy_policy.tsx
+++ src/components/faq_components/privacy_policy.tsx
@@ -6,7 +6,7 @@ export default function PrivacyPolicy(){
         <div className="border-b-[3px] border-b-gray-300 pb-4 lg:pb-12">
             
             <h1 className={`oswald ${h3FAQ} xl:pt-20 lg:pt-12 xs:pt-7 pt-4 text-dark_violet `}>Privacy Policy</h1>
-            <p className={`'arial lg:my-[13px] xl:leading-[34px]
+            <p className={`lg:my-[13px] xl:leading-[34px]
             text-light_violet font-[500] leading-[30px] align-baseline bg-transparent md:text-[19px] 
             xs:text-[18px] text-base`}>
                 For a personalised reading these data and info will be collected with high confidentiality for the reader:
